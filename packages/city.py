31d124736ecd7576726b0b8e3630f37dc90758fe
bhaveshlokhande-000 <72037462+bhaveshlokhande-000@users.noreply.github.com>
2021-07-31 21:19:38 +0530

    Add files via upload

---
+++ packages/city.py
@@ -0,0 +1,11 @@
+def city_name():
+    return "Mumbai";
+
+def city_population():
+    return 1000000000;
+
+def city_state():
+    return "Maharashtra";
+
+if __name__ == "__main__":
+    print("in module city");
